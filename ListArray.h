734ad652c638e175073cadf3a07225c2e172fcbc
paulacarcel <paulacarcel@DESKTOP-IL052TH.localdomain>
2025-10-21 19:55:10 +0200

    Añadido Makefile y código de test de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -2,6 +2,7 @@
 #define LISTARRAY_H
 
 #include <iostream>
+#include <stdexcept>  // Para std::out_of_range
 #include "List.h"
 
 template <typename T>
@@ -11,85 +12,114 @@ private:
     T* arr;
     int max;
     int n;
-    static const int MINSIZE;
+    static const int MINSIZE = 2;
 
-    void resize(int new_size);
+    void resize(int new_size) {
+        if (new_size < MINSIZE)
+            new_size = MINSIZE;
+
+        T* new_arr = new T[new_size];
+        for (int i = 0; i < n; ++i)
+            new_arr[i] = arr[i];
+
+        delete[] arr;
+        arr = new_arr;
+        max = new_size;
+    }
 
 public:
-    ListArray();
-    ~ListArray();
+    // Constructor
+    ListArray() {
+        max = MINSIZE;
+        n = 0;
+        arr = new T[max];
+    }
 
-    T operator[](int pos);
-    
+    // Destructor
+    ~ListArray() {
+        delete[] arr;
+    }
+
+    // Operador []
+    T operator[](int pos) {
+        if (pos < 0 || pos >= n) throw std::out_of_range("Posición inválida");
+        return arr[pos];
+    }
+
+    // Insertar un elemento en una posición
+    void insert(int pos, T e) override {
+        if (pos < 0 || pos > n) throw std::out_of_range("Posición inválida");
+
+        if (n == max)
+            resize(2 * max);
+
+        for (int i = n; i > pos; --i)
+            arr[i] = arr[i - 1];
+
+        arr[pos] = e;
+        n++;
+    }
+
+    // Agregar al final
+    void append(T e) override {
+        insert(n, e);
+    }
+
+    // Agregar al principio
+    void prepend(T e) override {
+        insert(0, e);
+    }
+
+    // Eliminar elemento por posición
+    T remove(int pos) override {
+        if (pos < 0 || pos >= n) throw std::out_of_range("Posición inválida");
+
+        T elem = arr[pos];
+        for (int i = pos; i < n - 1; ++i)
+            arr[i] = arr[i + 1];
+
+        n--;
+        if (max >= 4 * n && max > MINSIZE)
+            resize(max / 2);
+
+        return elem;
+    }
+
+    // Obtener elemento
+    T get(int pos) override {
+        if (pos < 0 || pos >= n) throw std::out_of_range("Posición inválida");
+        return arr[pos];
+    }
+
+    // Buscar elemento
+    int search(T e) override {
+        for (int i = 0; i < n; ++i)
+            if (arr[i] == e) return i;
+        return -1;
+    }
+
+    // Saber si está vacío
+    bool empty() override {
+        return n == 0;
+    }
+
+    // Saber tamaño actual
+    int size() override {
+        return n;
+    }
+
+    // Operador <<
     template <typename U>
-    friend std::ostream& operator<<(std::ostream &out, const ListArray<U> &list);
-
-    void insert(int pos, T e) override;
-    void append(T e) override;
-    void prepend(T e) override;
-    T remove(int pos) override;
-
-    T get(int pos) override;
-    int search(T e) override;
-    bool empty() override;
-    int size() override;
+    friend std::ostream& operator<<(std::ostream &out, const ListArray<U> &list) {
+        out << "[";
+        for (int i = 0; i < list.n; ++i) {
+            out << list.arr[i];
+            if (i < list.n - 1) out << ", ";
+        }
+        out << "]";
+        return out;
+    }
 };
 
-// ---------------- IMPLEMENTACIONES ----------------
-
-// Valor mínimo de tamaño
-template <typename T>
-const int ListArray<T>::MINSIZE = 2;
-
-// Constructor
-template <typename T>
-ListArray<T>::ListArray() {
-    max = MINSIZE;
-    n = 0;
-    arr = new T[max];
-}
-
-// Destructor
-template <typename T>
-ListArray<T>::~ListArray() {
-    delete[] arr;
-}
-
-// Método privado resize
-template <typename T>
-void ListArray<T>::resize(int new_size) {
-    if (new_size < MINSIZE)
-        new_size = MINSIZE;
-
-    T* new_arr = new T[new_size];
-
-    for (int i = 0; i < n; ++i)
-        new_arr[i] = arr[i];
-
-    delete[] arr;
-    arr = new_arr;
-    max = new_size;
-}
-template <typename T>
-T ListArray<T>::get(int pos) {
-    if (pos < 0 || pos >= n) throw std::out_of_range("Posición inválida");
-    return arr[pos];
-}
-
-template <typename T>
-int ListArray<T>::search(T e) {
-    for (int i = 0; i < n; ++i)
-        if (arr[i] == e) return i;
-    return -1;  // No encontrado
-}
-
-template <typename T>
-bool ListArray<T>::empty() {
-    return n == 0;
-}
-
-template <typename T>
-int ListArray<T>::size() {
-    return n;
-}
 #endif
+
